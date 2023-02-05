e3821e10d7959496db601c0cbebbe2cd5da39e7c
Saurabh <saurabhnegi0007@gmail.com>
2023-02-05 13:15:58 +0530

    added Coach Login functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './components/Home'
 import './App.css';
 import CoachSignup from './components/CoachSignup';
+import CoachLogin from './components/CoachLogin';
 
 function App() {
   return (
     
-      <CoachSignup />
+      <CoachLogin />
     
   );
 }
--- src/components/CoachLogin.js
+++ src/components/CoachLogin.js
@@ -1,25 +1,92 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const CoachLogin = () => {
-    return (
-        <>
-            <nav className="flex justify-between bg-[#111] text-white p-4">
-                    <h1 className="font-bold text-xl">WeCare</h1>
-                    <p>Call Us: 123 123434443</p>
-            </nav>
+  const [error, setError] = useState();
+  const [status, setStatus] = useState();
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
 
-            <div>
-                <div>
+  function HandleSubmit(event) {
+    event.preventDefault();
+    if (id.length == 0 || pwd.length < 5 || pwd.length > 10) {
+      setError(true);
+    }
+    
+    if (!(id.length == 0 || pwd.length < 5 || pwd.length > 10)) {
+      axios
+        .get("http://localhost:3000/coaches" + "?id=" + id + "&password=" + pwd)
+        .then((res) => {
+          console.log(res.data);
+          if(res.data.length>0) setStatus(false);
+          else setStatus(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+  return (
+    <>
+      <nav className="flex justify-between bg-[#111] text-white p-4">
+        <h1 className="font-bold text-xl">WeCare</h1>
+        <p>Call Us: 123 123434443</p>
+      </nav>
 
-                </div>
+      <div className="border-2 border-black w-1/6 mx-auto mt-2">
+        <div>
+          <h1>Login As Life Coach</h1>
+        </div>
 
-                <form>
-                    <input type="text" className="border-2 border-black block" placeholder="Coach id"/>
-                    <input type="password" className="border-2 border-black block" placeholder="Password"/>
+        <form>
+          <input
+            type="text"
+            onChange={(event) => {
+              setId(event.target.value);
+            }}
+            className="border-2 border-black block"
+            placeholder="Coach id"
+          />
+          {error && id.length == 0 ? (
+            <label className="block text-red-600">Coach id is required</label>
+          ) : (
+            ""
+          )}
 
-                    <button type="submit" className="border-2 border-black block cursor-pointer">Login</button>
-                </form>
-            </div>
-        </>
-    )
-}
+          <input
+            type="password"
+            onChange={(event) => {
+              setPwd(event.target.value);
+            }}
+            className="border-2 border-black block"
+            placeholder="Password"
+          />
+          {error && (pwd.length < 5 || pwd.length > 10) ? (
+            <label className="block text-red-600">
+              Password should have 5 to 10 characters
+            </label>
+          ) : (
+            ""
+          )}
+
+          <button
+            type="submit"
+            onClick={HandleSubmit}
+            className="border-2 border-black block cursor-pointer"
+          >
+            Login
+          </button>
+          {status?
+          <label>Invalid Credentials</label>:""}
+          
+          {status==false?
+          <label>Logged In</label>:""}
+
+          
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default CoachLogin;
